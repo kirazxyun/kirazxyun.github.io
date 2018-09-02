4aa4460d7fe02f9cf34f4ae519ea36d0a99fafe8
zhangxiaoyun <zhangxiaoyun0828@sina.cn>
2018-09-02 12:28:35 +0800

    add vue/component

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -64,7 +64,8 @@ module.exports = {
         children: [
           '',
           'data',
-          'transfer'
+          'transfer',
+          'component'
         ]
       }],
       '/webpack/': [{
